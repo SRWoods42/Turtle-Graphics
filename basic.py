ccf79b5857ca614038b9aa6a8b7256e876a07d20
Steve Woods <srwoods42@gmail.com>
2022-06-28 12:26:49 +0100

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+bgcolor("orange")
+pensize(2)
+
+
+forward(100)
+right(90)
+forward(100)
+
+
---
+++ square flower.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+bgcolor("orange")
+pensize(2)
+
+#speed(15)
+
+begin_fill()
+
+for counter_b in range(1000):
+    right(10)
+
+    for counter_a in range(4):
+        forward(100)
+        right(90)
+
+
+end_fill()
